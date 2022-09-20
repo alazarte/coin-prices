4e3f4b8c76e39319518fb71a8f0524d9d2618e00
alazarte <inbox@alazarte.com>
2022-09-20 11:20:44 -0300

    Organize code a bit and add default path for config and flag

--- cmd/cpcli/main.go
+++ cmd/cpcli/main.go
@@ -12,10 +12,34 @@ var (
 	coinfrom string
 	cointo   string
 
-        coinConfig api.CoinConfig
+	configFilepath string
+	coinConfig api.CoinConfig
 )
 
 func init() {
+	configureUsage()
+	configureFlags()
+	validateFlags()
+
+	cfg, err := createApiInstance(configFilepath)
+	if err != nil {
+		fmt.Printf("Error instancing api: %s\n", err)
+		os.Exit(1)
+	}
+	coinConfig = cfg
+}
+
+func main() {
+	if exch != 0 && cointo != "" {
+		getAndPrintExchange()
+		os.Exit(0)
+	} else if exch == 0 && cointo == "" {
+		getAndPrintPrice()
+		os.Exit(0)
+	}
+}
+
+func configureUsage() {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
@@ -26,44 +50,69 @@ Add -t and -d to get price exchange
 `)
 	}
 
+}
+
+func configureFlags() {
 	flag.Float64Var(&exch, "d", 1, "Specify amount to convert")
 	flag.StringVar(&coinfrom, "f", "usd", "Specify coin (BTC, ETH, ...)")
 	flag.StringVar(&cointo, "t", "usd", "To use with -d (BTC, ETH, USD, ...)")
+	flag.StringVar(&configFilepath, "c", "", "Config filepath in json")
 	flag.Parse()
 
-        // TODO validate arguments here
-
-        data, err := os.ReadFile("config.json")
-        if err != nil {
-                panic(err)
-        }
-
-        coinConfig, err = api.FromJSON(data)
-        if err != nil {
-                panic(err)
-        }
 }
 
-func main() {
-	if coinfrom != "" {
-		if exch != 0 && cointo != "" {
-			// TODO make this from cmdline
-			f, err := coinConfig.GetExch(coinfrom, cointo, exch)
-			if err != nil {
-				fmt.Printf("ERROR: %s\n", err)
-				os.Exit(1)
-			}
-			fmt.Printf("%.8f %s are %.8f %s\n", exch, coinfrom, f, cointo)
-			os.Exit(0)
-		} else if exch == 0 && cointo == "" {
-			price, err := coinConfig.GetPrice(coinfrom)
-			if err != nil {
-				fmt.Printf("ERROR: %s\n", err)
-				os.Exit(1)
-			}
-			fmt.Println(price)
-			os.Exit(0)
+func validateFlags() {
+	if coinfrom == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if configFilepath == "" {
+		if configFilepath = searchAndGetConfigFilepath(); configFilepath == "" {
+			fmt.Println("No config file found")
+			os.Exit(1)
 		}
 	}
-	flag.Usage()
+}
+
+func createApiInstance(filepath string) (api.CoinConfig, error) {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		fmt.Printf("Error reading config file: %s\n", err)
+		os.Exit(1)
+	}
+
+	return api.FromJSON(data)
+}
+
+func getAndPrintExchange() {
+	// TODO make this from cmdline
+	f, err := coinConfig.GetExch(coinfrom, cointo, exch)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%.8f %s are %.8f %s\n", exch, coinfrom, f, cointo)
+}
+
+func getAndPrintPrice() {
+	price, err := coinConfig.GetPrice(coinfrom)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(price)
+}
+
+func searchAndGetConfigFilepath() string {
+	homepath := os.Getenv("HOME")
+	if homepath == "" {
+		return ""
+	}
+
+	formatPath := fmt.Sprintf("%s/.config/coin_prices.json", homepath)
+	if _, err := os.Stat(formatPath); err != nil {
+		return ""
+	}
+
+	return formatPath
 }
